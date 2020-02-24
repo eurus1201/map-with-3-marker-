88dcadccdf21b8662cf14a4df038f74527668b1b
khodabakhshian <khodabakhshian.s@gmail.com>
2020-02-24 10:18:37 +0330

    added google map api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Fragment } from "react";
+import {useLoadScript,  GoogleMap, Marker, InfoWindow} from "@react-google-maps/api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+ function App() {
+  const [mapRef, setMapRef] = useState(null);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [markerMap, setMarkerMap] = useState({});
+  const [center, setCenter] = useState({ lat: 35.715298 , lng:51.404343  });
+  const [zoom, setZoom] = useState(5);
+  const [clickedLatLng, setClickedLatLng] = useState(null);
+  const [infoOpen, setInfoOpen] = useState(false);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyD9i8oKXeMnrsnp16jq2sWO62PAhlgSdJM"
+  });
+
+
+  const myPlaces = [
+    { id: "place1", pos: { lat: 35.715298  , lng: 51.404343 } },
+    { id: "place2", pos: { lat: 35.715398 , lng: 51.404343 } },
+    { id: "place3", pos: { lat: 35.715498 , lng: 51.404343 } }
+  ];
+
+  const fitBounds = map => {
+    const bounds = new window.google.maps.LatLngBounds();
+    myPlaces.map(place => {
+      bounds.extend(place.pos);
+      return place.id;
+    });
+    map.fitBounds(bounds);
+  };
+
+  const loadHandler = map => {
+    setMapRef(map);
+    fitBounds(map);
+  };
+
+  const markerLoadHandler = (marker, place) => {
+    return setMarkerMap(prevState => {
+      return { ...prevState, [place.id]: marker };
+    });
+  };
+
+  const markerClickHandler = (event, place) => {
+    setSelectedPlace(place);
+
+    if (infoOpen) {
+      setInfoOpen(false);
+    }
+
+    setInfoOpen(true);
+
+    if (zoom < 13) {
+      setZoom(13);
+    }
+
+  };
+
+  const renderMap = () => {
+    return (
+      <Fragment>
+        <GoogleMap
+          onLoad={loadHandler}
+          // onCenterChanged={() => setCenter(mapRef.getCenter().toJSON())}
+          onClick={e => setClickedLatLng(e.latLng.toJSON())}
+          center={center}
+          zoom={zoom}
+          mapContainerStyle={{
+            height: "70vh",
+            width: "100%"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {myPlaces.map(place => (
+            <Marker
+              key={place.id}
+              position={place.pos}
+              onLoad={marker => markerLoadHandler(marker, place)}
+              onClick={event => markerClickHandler(event, place)}
+              icon={{
+                path:
+                  "M12.75 0l-2.25 2.25 2.25 2.25-5.25 6h-5.25l4.125 4.125-6.375 8.452v0.923h0.923l8.452-6.375 4.125 4.125v-5.25l6-5.25 2.25 2.25 2.25-2.25-11.25-11.25zM10.5 12.75l-1.5-1.5 5.25-5.25 1.5 1.5-5.25 5.25z",
+                fillColor: "#0000ff",
+                fillOpacity: 1.0,
+                strokeWeight: 0,
+                scale: 1.25
+              }}
+            />
+          ))}
+
+          {infoOpen && selectedPlace && (
+            <InfoWindow
+              anchor={markerMap[selectedPlace.id]}
+              onCloseClick={() => setInfoOpen(false)}
+            >
+              <div>
+                <h3>{selectedPlace.id}</h3>
+                <div>This is your info window content</div>
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+
+        <h3>
+          Center {center.lat}, {center.lng}
+        </h3>
+
+        {clickedLatLng && (
+          <h3>
+            You clicked: {clickedLatLng.lat}, {clickedLatLng.lng}
+          </h3>
+        )}
+
+        {selectedPlace && <h3>Selected Marker: {selectedPlace.id}</h3>}
+      </Fragment>
+    );
+  };
+
+  return isLoaded ? renderMap() : null;
 }
 
 export default App;
